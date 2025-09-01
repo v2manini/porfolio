f31f28bd3b65794d889086ab6176675a2f1fa006
VM <VM@server.fake>
2025-09-01 00:56:21 -0300

    Se cambió el @import y la lista de lenguajesse puso en otro archivo.

--- src/components/Skill.svelte
+++ src/components/Skill.svelte
@@ -1,49 +1,7 @@
 <script>
     import ContenedorSkill from './ContenedorSkill.svelte';    //https://es.m.wikipedia.org/wiki/Archivo:HTML5_logo_and_wordmark.svg
-    import {Reobjet} from "./js/convineProgs"
-
-    const fronted = [
-        {
-            lenguaje: "HTML",
-            img: "https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg",
-        },
-        {
-            lenguaje: "CSS",
-            img: "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg",
-        },
-        {
-            lenguaje: "Sass",
-            img: "https://cdn-icons-png.flaticon.com/512/5968/5968358.png   ",
-        },
-        {
-            lenguaje: "JS",
-            img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
-        },
-        {
-            lenguaje: "SVELTE",
-            img: "http://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
-        },
-    ];
-    const backend = [
-        {
-            lenguaje: "Nodejs",
-            img: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
-        },
-        {
-            lenguaje: "Mysql",
-            img: "/imgs/Database-mysql.svg",
-        },
-        {
-            lenguaje: "SVELTE KIT",
-            img: "http://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
-        },
-    ];
-    const lowlevel = [
-        {
-            lenguaje: "C",
-            img: "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
-        },
-    ];
+    // import {Reobjet} from "./js/convineProgs"
+    import {fronted,backend,lowlevel} from "./js/lenguajes"
 
     // let todos = [];
 
@@ -62,5 +20,4 @@
     <ContenedorSkill lenguajes = {fronted} MainTitle="Fronted"/>
     <ContenedorSkill lenguajes = {backend} MainTitle="Backend"/>
     <ContenedorSkill lenguajes = {lowlevel} MainTitle="Bajo nivel"/>
-
 </div>
---
+++ src/components/js/lenguajes.js
@@ -0,0 +1,44 @@
+const fronted = [
+  {
+    lenguaje: "HTML",
+    img: "https://upload.wikimedia.org/wikipedia/commons/3/38/HTML5_Badge.svg",
+  },
+  {
+    lenguaje: "CSS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/62/CSS3_logo.svg",
+  },
+  {
+    lenguaje: "Sass",
+    img: "https://cdn-icons-png.flaticon.com/512/5968/5968358.png   ",
+  },
+  {
+    lenguaje: "JS",
+    img: "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  },
+  {
+    lenguaje: "SVELTE",
+    img: "http://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+  },
+];
+const backend = [
+  {
+    lenguaje: "Nodejs",
+    img: "https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg",
+  },
+  {
+    lenguaje: "Mysql",
+    img: "/imgs/Database-mysql.svg",
+  },
+  {
+    lenguaje: "SVELTE KIT",
+    img: "http://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg",
+  },
+];
+const lowlevel = [
+  {
+    lenguaje: "C",
+    img: "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+  },
+];
+
+export { fronted, backend, lowlevel };
