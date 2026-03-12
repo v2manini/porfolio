9074adac683815a0c3684e5b1483e53ea9f4ce63
VM <VM@server.fake>
2026-03-11 23:42:35 -0300

    ??????

--- src/content/proyects/config.js
+++ src/content/proyects/config.js
@@ -1,9 +1,7 @@
 import { defineCollection} from "astro:content";
 import { z } from 'astro/zod';
-import { glob } from 'astro/loaders';
 
 const proyects = defineCollection({
-    loader: glob({ pattern: "**/*.md", base: "./src/content/proyects" }),
     schema: z.object({
         title:z.string(),
         img: z.url(),
